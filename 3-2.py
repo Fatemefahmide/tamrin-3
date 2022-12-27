a88d55271a559758f4c8a98206fdd53956b381c7
fateme fahmide <120297428+Fatemefahmide@users.noreply.github.com>
2022-12-28 00:28:47 +0330

    Add files via upload

---
+++ 3-2.py
@@ -0,0 +1,15 @@
+a = int(input("rows: "))
+b = int(input("cols: "))
+c = 1
+
+for i in range(1,1+a):
+       for j in range(1,1+b):
+              print(i,j ,end="*")
+              if c ==1:
+                     print(end=(" "))
+              else:
+                     print(end=(" "))
+       
+       print( )
+
+
